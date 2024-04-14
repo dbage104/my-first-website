22cd9bfa5899511d61e659289bd5b158966f01b2
dryden brown <dbgage104@gmail.com>
2024-04-13 17:42:11 -0700

    added javascript and jquary

---
+++ java_script/global.js
@@ -0,0 +1,13 @@
+function clearform()
+{document.getElementById('survey form').reset();}
+
+// javascript test code comentted for referance linked to index
+
+// function hideptags(){
+//     let ptags = document.getElementsByTagName ('p');
+//     for(let item of ptags){
+//         item.style.display = 'none';
+//     }}
+
+// $('#jquaryclear').click(function(){
+//     $('p').hide();})
